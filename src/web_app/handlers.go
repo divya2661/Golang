721b69f0bf0c939870a8c452b1f460a736a9fca8
Divya <divya.nagar@juspay.in>
2016-07-04 02:28:27 +0530

    web app rest api complete

--- src/web_app/handlers.go
+++ src/web_app/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"encoding/json"
+	"io/ioutil"
+	"io"
 )
 
 func Index (w http.ResponseWriter, r *http.Request){
@@ -12,11 +14,8 @@ func Index (w http.ResponseWriter, r *http.Request){
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request){
-	type Todos []Todo
-	todos := Todos{
-		Todo{Name: "Write Presentation"},
-		Todo{Name: "Host meetup"},
-	}
+	w.Header().Set("Content-Type","application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err:= json.NewEncoder(w).Encode(todos); err !=nil {
 		panic(err)
 	}
@@ -27,4 +26,32 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	todoId  := vars["todoId"]
 	fmt.Fprintln(w,"Todo Show: ",todoId)
+}
+
+func TodoCreate(w http.ResponseWriter, r *http.Request){
+	
+	var todo Todo
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body,1048576))
+	if err != nil{
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil{
+		panic(err)
+	}
+	fmt.Fprintln(w,json.Unmarshal(body,&todo))
+	if err := json.Unmarshal(body,&todo);err != nil{
+		w.Header().Set("Content-Type","application/json;charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err!=nil{
+			panic(err)
+		}
+	} 
+
+	t := RepoCreateTodo(todo)
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusCreated)
+    if err := json.NewEncoder(w).Encode(t); err != nil {
+        panic(err)
+    }
+
 }
